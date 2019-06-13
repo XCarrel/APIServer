662fe3cfe99ddfc78c596a5d2641de9a880c6ec5
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-06-13 13:25:02 +0200

    Fixed merge errors

--- app/NHYAccount.php
+++ app/NHYAccount.php
@@ -6,25 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class NHYAccount extends Model
 {
-<<<<<<< HEAD
     public $table = "NHY_Accounts";
 
     public function transactions()
     {
         return $this->hasMany('App\NHYTransaction', 'account_id');
-=======
-    public $table = "NHY_accounts";
-    public $timestamps = false;
-
-    protected $fillable = [
-        'name',
-        'favorite',
-        'value',
-    ];
-
-    public function nhy_transactions()
-    {
-        return $this->hasMany('App\NHYTransactions');
->>>>>>> 9cf69fb856f1a5e9396432d7c97fab1eaf9fc15f
     }
 }
